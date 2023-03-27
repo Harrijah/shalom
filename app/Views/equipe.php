c19ea3a1a94c2a9c20191ebc306e448caf81ad60
Hari <113083576+Harrijah@users.noreply.github.com>
2023-03-27 14:54:44 +0300

    MAJ page about us

--- app/Views/equipe.php
+++ app/Views/equipe.php
@@ -1,38 +1,45 @@
 <section>
     <h2 style="text-align:center;">Notre équipe</h2>
     <div class="separator01"></div>
-    <div class="bann-content">
-        <!-- <div class="grid-me">
-                <div class="vide"></div> -->
-            <div class="equipe border-me">
-                <img src="<?php echo base_url('image/dr.png'); ?>" alt="docteur nutrithérapeute" class="standard-image prez02" style="border-bottom: double 4px var(--color07); max-width: 350px;">   
-                <h3>Un médecin  coordonnateur et gérante du centre</h3>
-                <ul>
-                <li>
-                    <span style="text-decoration: underline;">Manager et Leader</span> : Cordonne, gère, organise et supervise toutes les activités du centre. Le médecin effectue le test de tous les entrants et en les classant selon leur état de santé. Il assure la formation de tout le personnel, privilégie le briefing et débriefing avec le personnel.
-                </li>
-                <li>
-                    <span style="text-decoration: underline;">Coach en Nutrithérapie</span> (notre aliment = notre médicament) : établit une alimentation équilibrée, apprend les couleurs de la santé, <b>établit une assiette antioxydant, une assiette anti-cancer</b>, fait connaître la pharmacie alimentaire, apprend les interactions alimentaires, établit le menu journalier adapté et individualisé.
-                </li>
-                <li>
-                    <span style="text-decoration: underline;">Coach en Santé Bien-être</span> : anime avec des activités sur le ralentissement du vieillissement, la rigologie, la réduction du stress, l'élimination des métaux lourds et  la prévention du dysfonctionnement hormonal, et des conseils sur "comment bien dormir".
-                </li>
-                <li>
-                    <span style="text-decoration: underline;">Assurer le développement psychosocial du patient en internat</span> : le travail est centré sur l’individu qui est acteur et participe à son propre épanouissement : le patient est « le sujet » et non « un objet ». Elle renforce la procuration du confort physique et psychologique et assure une prise en charge sociale et spirituelle.
-                </li>
-                <li>
-                    <span style="text-decoration: underline;">Assurer la stimulation cognitive</span> : par différents tests et jeux de mémoire, par la Réminiscence. Une des activités consiste à créer des moments d’échange sur leur histoire et maintenir les capacités intellectuelles des résidents du centre.
-                </li>
-                <li>
-                    <span style="text-decoration: underline;">Assurer et superviser l’animation en permanence</span> : elle assure l'élaboration des projets du centre, garantit le bon déroulement des activités, assure la diversification des activités afin d'éviter la routine. Elle supervise les différents intervenants, planifie les rencontres programmés et intergénérationnelles, et effectue le bilan mensuel de toutes les activités.
-                </li>
-                </ul>
-            </div>
-            
-            <div class="separator01 sep"></div>
+        <div class="bann-content03 border-me03">
+            <div style="margin-bottom: 35px;">
 
-            <div class="">
-                <h3>Trois animatrices</h3>
+            
+                <h3 style="text-align: center;">Un médecin  coordonnateur et gérante du centre</h3>
+                <div>
+                    <img src="<?php echo base_url('image/dr02.fw.png'); ?>" alt="docteur nutrithérapeute" class="standard-image prez02" style="border-bottom: solid 4px var(--color07); width: 350px; height: auto;">   
+                </div>
+                <div class="equipe02">
+                    <ul>
+                    <li>
+                        <span style="text-decoration: underline;">Manager et Leader</span> : Cordonne, gère, organise et supervise toutes les activités du centre. Le médecin effectue le test de tous les entrants et en les classant selon leur état de santé. Il assure la formation de tout le personnel, privilégie le briefing et débriefing avec le personnel.
+                    </li>
+                    <li>
+                        <span style="text-decoration: underline;">Coach en Nutrithérapie</span> (notre aliment = notre médicament) : établit une alimentation équilibrée, apprend les couleurs de la santé, <b>établit une assiette antioxydant, une assiette anti-cancer</b>, fait connaître la pharmacie alimentaire, apprend les interactions alimentaires, établit le menu journalier adapté et individualisé.
+                    </li>
+                    <li>
+                        <span style="text-decoration: underline;">Coach en Santé Bien-être</span> : anime avec des activités sur le ralentissement du vieillissement, la rigologie, la réduction du stress, l'élimination des métaux lourds et  la prévention du dysfonctionnement hormonal, et des conseils sur "comment bien dormir".
+                    </li>
+                    <li>
+                        <span style="text-decoration: underline;">Assurer le développement psychosocial du patient en internat</span> : le travail est centré sur l’individu qui est acteur et participe à son propre épanouissement : le patient est « le sujet » et non « un objet ». Elle renforce la procuration du confort physique et psychologique et assure une prise en charge sociale et spirituelle.
+                    </li>
+                    <li>
+                        <span style="text-decoration: underline;">Assurer la stimulation cognitive</span> : par différents tests et jeux de mémoire, par la Réminiscence. Une des activités consiste à créer des moments d’échange sur leur histoire et maintenir les capacités intellectuelles des résidents du centre.
+                    </li>
+                    <li>
+                        <span style="text-decoration: underline;">Assurer et superviser l’animation en permanence</span> : elle assure l'élaboration des projets du centre, garantit le bon déroulement des activités, assure la diversification des activités afin d'éviter la routine. Elle supervise les différents intervenants, planifie les rencontres programmés et intergénérationnelles, et effectue le bilan mensuel de toutes les activités.
+                    </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="separator01"></div>
+
+            <div class="equipe03">
+                <h3 style="text-align: center;">Trois animatrices</h3>
+            <div>
+                <img src="<?php echo base_url('image/assistante.jpg'); ?>" alt="assistante" class="standard-image prez02 border-me" style="width: 350px; height: auto;">   
+            </div>
                 <ul>
                     <li>Rôle occupationnel orienté sur des activités thérapeutiques</li>
                     <li>Dynamiser les activités</li>
@@ -51,18 +58,21 @@
                     <li>Maintenir et réhabiliter les liens sociaux des personnes âgées vieillissantes</li>
                 </ul>
             </div>
-        <!-- </div> -->
-        
-        <div class="equipe border-me">
-            <h3>Un Directeur artistique</h3>
-            <p style="margin: 0 20px">
-                Le rôle du directeur artistique chez Shalom - Maison d'Accueil est de créer un environnement stimulant pour les résidents en favorisant des activités créatives telles que la peinture, le chant et la musique. Il vise à stimuler les cinq sens et à encourager les résidents à exprimer leur créativité tout en réduisant les troubles du comportement et l'isolement. Le directeur artistique respecte le rythme et les choix des résidents, tout en cherchant à leur offrir des moments de bonheur et de connexion avec le monde qui les entoure.
-            </p>
-        </div>
-        <div class="bann-content02">
 
+            <div class="separator01"></div>
+        
+            <div class="equipe03">
+                <h3 style="text-align: center;">Un Directeur artistique</h3>
+                <p style="margin: 0 20px">
+                    Le rôle du directeur artistique chez Shalom - Maison d'Accueil est de créer un environnement stimulant pour les résidents en favorisant des activités créatives telles que la peinture, le chant et la musique. Il vise à stimuler les cinq sens et à encourager les résidents à exprimer leur créativité tout en réduisant les troubles du comportement et l'isolement. Le directeur artistique respecte le rythme et les choix des résidents, tout en cherchant à leur offrir des moments de bonheur et de connexion avec le monde qui les entoure.
+                </p>
+            </div>
+
+            <div class="separator01"></div>
+
+        <div class="bann-content04">
             <div class="equipe border-me">
-                <h3>Un personnel administratif</h3>
+                <h3 style="text-align: center;">Un personnel administratif</h3>
                 <ul>
                     <li>Assurer toutes les tâches administratives</li>
                     <li>Créer et Classer tous les dossiers des patients</li>
@@ -73,9 +83,8 @@
                     <li>Organiser un voyage en mer chaque année validée par le gérant</li>
                 </ul>
             </div>
-            
             <div class="equipe border-me">
-                <h3>Trois assistantes</h3>
+                <h3 style="text-align: center;">Trois assistantes</h3>
                 <ul>
                         <li>Assurer les moments de lecture (les nouvelles du jour et revue de presse)</li>
                         <li>Elaborer l’atelier de cuisine (préparation des crudités du midi)</li>
@@ -87,19 +96,17 @@
                 </ul>
             </div>
         </div>
-
-        <div class="bann-content02">
+        <div class="bann-content04">
             <div class="equipe border-me">
-                <h3>Un coach en Zumba senior</h3>
+                <h3 style="text-align: center;">Un coach en Zumba senior</h3>
                 <ul>
                     <li>En étroite collaboration avec le coach de gymnastique douce</li>
                     <li>Apprendre des mouvements adaptés et coordonnés sur une chaise ou non (en fonction de l’âge)</li>
                     <li>Les associer à des chants et danses</li>
                 </ul>
             </div>
-            
             <div class="equipe border-me">
-                <h3 style="display: inline-block">Un coach en Gym douce </h3><span> (Séance spéciale pour les Ainés)</span>
+                <h3 style="text-align: center;">Un coach en Gym douce </h3><span></span>
                 <ul>
                     <li>Exercices musculaires</li>
                     <li>Exercices de redressement et Equilibre</li>
@@ -108,5 +115,16 @@
             </div>
         </div>
 
-    </div>
+
+
+
+
+
+
+
+
+
+        </div>
+
+    <!-- </div> -->
 </section>
