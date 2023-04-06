10d0de452b5ec78c13a0febd7587aa99a750a2f3
harrijah <andrianarivohari@gmail.com>
2023-04-06 17:48:41 +0200

    formulaire validate

--- app/Controllers/Contact.php
+++ app/Controllers/Contact.php
@@ -22,21 +22,52 @@
 
             $email = \Config\Services::email();
 
-            $email->setFrom('noreply@shalom.mg', 'Site web Shalom');
-            $email->setTo('contact@shalom.mg');
-            // $email->setCC($data['email'], 'contact@shalom.mg');
-            $email->setBCC('andrianarivohari@gmail.com');
-            $email->setSubject('Demande de la part de ' . $data['nom']);
-            $email->setMessage(
-                'Message de '. $data['nom'] . 
-                'Téléphone : '. $data['telephone'] . 
-                $data['message']
-            );
 
-            $email->send();
-            return redirect()->to('/');
-            //$email->printDebugger(['headers']);
-			//var_dump($data);
+            if($this->request->getMethod() == 'get'){
+                $rules = [
+                    'email' => 'required'|valid_email,
+                    'telephone' => 'required'|max_length[15],
+                    'nom' => 'required',
+                    'message' => 'required',
+
+                ];
+
+                $errors = [
+                    'email' => [
+                        'validateUser' => 'Veuillez remplir par votre adresse e-mail.'
+                    ];
+                    'telephone' => [
+                        'validateUser' => 'Veuillez remplir par un numéro de téléphone valide.'
+                    ];
+                    'nom' => [
+                        'validateUser' => 'Veuillez renseigner votre nom.'
+                    ];
+                    'message' => [
+                        'validateUser' => 'Vous avez oublié d\'écrire votre message.'
+                    ];
+                ];
+            }
+
+            if(!$this->validate($rules, $errors)){
+                $data['validation'] = $this->validator
+            } else {
+
+                $email->setFrom('noreply@shalom.mg', 'Site web Shalom');
+                $email->setTo('contact@shalom.mg');
+                // $email->setCC($data['email'], 'contact@shalom.mg');
+                $email->setBCC('andrianarivohari@gmail.com');
+                $email->setSubject('Demande de la part de ' . $data['nom']);
+                $email->setMessage(
+                    'Message de '. $data['nom'] . 
+                    'Téléphone : '. $data['telephone'] . 
+                    $data['message']
+                );
+                
+                $email->send();
+                return redirect()->to('/');
+                //$email->printDebugger(['headers']);
+                //var_dump($data);
+            }
         }
 
 
